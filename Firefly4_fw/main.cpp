e3b736d2105a584fea3a7a51364c3ca36c4f9ca4
Kreyl <laystoll@yandex.ru>
2015-12-26 01:23:26 +0300

    + FW started

---
+++ Firefly4_fw/main.cpp
@@ -0,0 +1,44 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 26 дек. 2015 г.
+ *      Author: Kreyl
+ */
+
+#include "main.h"
+
+int main(void) {
+
+    // ==== Setup clock frequency ====
+//    uint8_t ClkResult = 1;
+//    SetupVCore(vcore1V8);
+//    Clk.SetupFlashLatency(32);  // Setup Flash Latency for clock in MHz
+//    Clk.SetupPLLMulDiv(pllMul8, pllDiv3);
+//    Clk.SetupBusDividers(ahbDiv1, apbDiv1, apbDiv1);
+//    ClkResult = Clk.SwitchToPLL();
+    Clk.UpdateFreqValues();
+
+    // Init OS
+    halInit();
+    chSysInit();
+
+    // ==== Init hardware ====
+    Uart.Init(256000, UART_GPIO, UART_TX_PIN, UART_GPIO, UART_RX_PIN);
+    Uart.Printf("\r%S %S\r", APP_NAME, APP_VERSION);
+
+//    Clk.PrintFreqs();
+//    if(ClkResult != 0) Uart.Printf("XTAL failure\r");
+
+    App.InitThread();
+
+    // Main cycle
+    App.ITask();
+}
+
+__attribute__ ((__noreturn__))
+void App_t::ITask() {
+    while(true) {
+        uint32_t EvtMsk = chEvtWaitAny(ALL_EVENTS);
+
+    } // while true
+}
---
+++ Firefly4_fw/main.h
@@ -0,0 +1,40 @@
+/*
+ * main.h
+ *
+ *  Created on: 26 дек. 2015 г.
+ *      Author: Kreyl
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include "ch.h"
+#include "kl_lib.h"
+#include "uart.h"
+#include "evt_mask.h"
+#include "board.h"
+
+#define APP_NAME            "Firefly4"
+#define APP_VERSION         __DATE__ " " __TIME__
+
+class App_t {
+private:
+    thread_t *PThread;
+public:
+    // Eternal methods
+    void InitThread() { PThread = chThdGetSelfX(); }
+    void SignalEvt(eventmask_t Evt) {
+        chSysLock();
+        chEvtSignalI(PThread, Evt);
+        chSysUnlock();
+    }
+    void SignalEvtI(eventmask_t Evt) { chEvtSignalI(PThread, Evt); }
+    void OnCmd(Shell_t *PShell);
+    // Inner use
+    void ITask();
+};
+
+extern App_t App;
+
+
+#endif /* MAIN_H_ */
